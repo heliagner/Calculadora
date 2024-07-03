d988cdd5da3fe0f83938e52da11d800e9853fb5b
heliagner <heliagner@hotmail.com>
2024-07-03 20:49:49 -0300

    Calculadora finalizada

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,12 @@ const prompt = require("prompt-sync")();
 
 let resultado;
 
+function verificarNumero(numero1, numero2){
+    if (isNaN(numero1) || isNaN(numero2));
+        console.log("Digite um número válido");
+        process.exit();
+}
+
 function adicao(numero1, numero2){
     return numero1 + numero2
 };
@@ -15,7 +21,7 @@ function multiplicacao(numero1, numero2){
 };
 
 function divisao(numero1, numero2){
-    if (numero2 !== 0){
+    if (numero2 === 0){
         console.log("Não existe número divisível por zero, tente novamente");
         } else {
 
@@ -24,26 +30,28 @@ function divisao(numero1, numero2){
 };
 
 function porcentagem(numero1, numero2){
-    return numero1 * numero2 / 100
+    return (numero1 * numero2) / 100
 };
 
 function calculadora(){
-    let numero1 = Number(prompt(
-                `~~~CALCULADORA COM 5 OPERAÇÕES~~~
-    1. Digite o primeiro número que deseja realizar a operação:`));
+let numero1, numero2, operacao;
 
-    let numero2 = Number(prompt(
-                `~~~CALCULADORA COM 5 OPERAÇÕES~~~
-    2. Digite o segundo número que deseja realizar a operação:`));
-    let operacao = prompt(
-                `~~~CALCULADORA COM 5 OPERAÇÕES~~~
-    Agora digite qual operação deseja realizar:
-                        (+) Adição
-                        (-) Subtração
-                        (*) Multiplicação
-                        (/) Divisão
-                        (%) porcentagem`);
+console.log(` 
+                ||| CALCULADORA SIMPLES |||
+            `)
+numero1 = verificarNumero(prompt(`1. Digite o primeiro número que deseja realizar a operação:`));
+numero2 = verificarNumero(prompt(`2. Digite o segundo número que deseja realizar a operação: `));
 
+
+console.log(`
+                Agora digite qual operação deseja realizar:
+                (+) Adição
+                (-) Subtração
+                (*) Multiplicação
+                (/) Divisão
+                (%) porcentagem`);
+
+operacao = prompt();
 
     switch (operacao) {
         case "+":
@@ -69,8 +77,8 @@ function calculadora(){
         default:
             console.log("Operação inválida, digite novamente!");
             break;
-    };
-};
+        };
+    }
 
 calculadora();
 
